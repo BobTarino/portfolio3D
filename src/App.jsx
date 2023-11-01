e714250d01ceb7187d2c0115c3e8af49842c310d
BobTarino <bob.tarino@gmail.com>
2023-11-01 19:55:28 -0400

    imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Tech, Works, StarsCanvas } from './components';
+
 const App = () => {
     return (
       <div>
-        3D Developer Portfolio
+        
       </div>
     )
   }
 
 export default App
 
-/* https://www.youtube.com/watch?v=0fYi8SGA20k  time stamp: 17:45 */
 
+/* https://www.youtube.com/watch?v=0fYi8SGA20k  time stamp: 17:45 */
