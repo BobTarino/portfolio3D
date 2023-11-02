f5e418a37789a040a3da727d859d79c6e59bd097
BobTarino <bob.tarino@gmail.com>
2023-11-02 15:47:05 -0400

    setting up nav bar w/ dynamic tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`} 
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/" /* direct to top of page */
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");  /* callback function; setActive set to nothing - keeps track of where user is on page */
+            window.scrollTo(0, 0); /* will scroll to top of page */
+          }}
+        >
+          <img />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
