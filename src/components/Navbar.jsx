eda3d4daf597656d357eff9d9b430675e3ac6e81
BobTarino <bob.tarino@gmail.com>
2023-11-02 16:30:53 -0400

    Customized personal logo and favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,15 @@ const Navbar = () => {
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to="/" /* direct to top of page */
+          to="/" /* direct to top of page */ 
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");  /* callback function; setActive set to nothing - keeps track of where user is on page */
             window.scrollTo(0, 0); /* will scroll to top of page */
-          }}
+          }} 
         >
-          <img />
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> 
+          <p className="text-white text-[18px] font-bold cursor-pointer">Robert <span className="sm:block hidden">| Tarino</span></p>
         </Link>
       </div>
     </nav>
