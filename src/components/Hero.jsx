9204b0b5edc9c97fee890e55b1ddb3a8a3c7687d
BobTarino <bob.tarino@gmail.com>
2023-11-04 17:59:28 -0400

    added sample sample subheader

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,9 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Robert</span></h1>
+          <p classname={`${styles.heroSubText} mt-2 text-white-100`}>
+            EDIT THIS! This is the about me section of who I am as a developer.
+          </p>
         </div>
       </div>
     </section>
