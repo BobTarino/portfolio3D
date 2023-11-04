5816a958a591497e069054b38d08be972a60e0c0
BobTarino <bob.tarino@gmail.com>
2023-11-04 19:37:56 -0400

    added hemisphereLoght and pointLight for 3d animation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,13 +1,30 @@
 import { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'; /*importing empty canvas to place something on  */
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'; /* import 3D models w/ useGLTF*/
 
 import CanvasLoader from '../Loader'; 
 
-const Computers = () => {
+const Computers = () => { /* 3d model for computer */
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} /* glare light on screen*//>
+      <primitive /* pass the object for the scene*/
+        object={computer.screen}
+      />
+    </mesh>
   )
+}
+
+const CompuerCanvas = () => {
+  return (
+    <Canvas>
+      
+    </Canvas>
+  )
+
 }
 
 export default Computers
