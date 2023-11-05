296640467121b287c59a92082ef6e9f8280734e9
BobTarino <bob.tarino@gmail.com>
2023-11-05 14:42:05 -0500

    used framer motion to make animated scroll button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,9 +25,17 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev>
-              
-            </motion.dev>
+            <motion.dev /* using framer-motion to animate scroll button */
+              animate={{
+                y: [0, 24, 0] /* move circle 24 pixels up and down */
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop' /* infinitly looped */
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
           </div>
         </a>
 
