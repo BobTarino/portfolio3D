ad4cd828ed841810414d2af4b7b1324f9ccbb445
BobTarino <bob.tarino@gmail.com>
2023-11-04 20:06:33 -0400

    installed original threejs package

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -27,8 +27,18 @@ const CompuerCanvas = () => {
       gL={{ preserveDrawingBuffer: true }} /* properly render model  */
     >
       <Suspense fallback={<CanvasLoader />} /* canvas loader while model is loading*/> 
-        <OrbitControls /*move model left or right*/ />
+        <OrbitControls /*move model left or right*/
+          enableZoom={false}
+          /* can only rotate model on a specific axis; more streamlined effect*/
+          maxPolarAngle={Math.PI/ 2}
+          minPolarAngle={Math.PI/ 2}
+        />
+        <Computers />
       </Suspense>
+
+      <Preload all />
+
+    
     </Canvas>
   )
 
