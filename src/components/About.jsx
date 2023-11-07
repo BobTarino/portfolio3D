4f07faf3095c03e7983a3cf0d34e2e80735e6b0b
BobTarino <bob.tarino@gmail.com>
2023-11-06 21:40:15 -0500

    edited about section overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
@@ -13,6 +13,12 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+      >
+        I'm a software developer with experience in C++ and MERN-stack Javascript (MongoDB, Express, React, & Node). Together, we can debug issues, find solutions, and create next-level projects.     
+      </motion.p>
     </>
   )
 }
