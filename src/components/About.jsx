d534300d4576b5dca6e20f7b39c98d4b16fc284c
BobTarino <bob.tarino@gmail.com>
2023-11-12 17:10:48 -0500

    used tailwind css to make cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,16 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-      
+        <div /* tilt options for animated cards */
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          
+        </div>
       </motion.div>
     </Tilt>
   )
