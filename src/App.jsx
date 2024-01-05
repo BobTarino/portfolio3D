f959560d58df28d90a5efa4ab8f2c814d85d93f9
BobTarino <bob.tarino@gmail.com>
2024-01-05 13:23:20 -0500

    import VerticalTimeline

--- src/App.jsx
+++ src/App.jsx
@@ -27,5 +27,5 @@ const App = () => {
 export default App
 
 
-/* https://www.youtube.com/watch?v=0fYi8SGA20k  time stamp: 1:11:42 */
+/* https://www.youtube.com/watch?v=0fYi8SGA20k  time stamp: 1:23:20 */
 
